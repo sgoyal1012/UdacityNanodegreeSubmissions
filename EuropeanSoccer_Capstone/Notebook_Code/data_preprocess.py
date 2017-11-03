6ba740fe2f58235123bd51adfa41649733f8f0f8
Sahil <sa.goyal@criteo.com>
2017-11-03 00:57:35 -0700

    Finishing util APIs

--- EuropeanSoccer_Capstone/Notebook_Code/data_preprocess.py
+++ EuropeanSoccer_Capstone/Notebook_Code/data_preprocess.py
@@ -32,12 +32,23 @@ def desc_table(table_df, table_name):
 
 def home_advantage(matches_df, conn):
   # TODO: Convert to DataFrama and Plot
-  country_id_to_num_matches = matches_df.groupby(
-      ['country_id']).size().to_dict()
+  country_id_to_num_matches = matches_df[['country_id', 'result_label']].copy()
+  grouped_df = country_id_to_num_matches.\
+    groupby(['country_id'], as_index=False).agg({'result_label':'count'}).rename(columns={'result_label': 'matches_per_country'})
+
+  win_df = country_id_to_num_matches[country_id_to_num_matches['result_label'] == 'HOME_WIN']
+  grouped_home_df = win_df. \
+    groupby(['country_id'], as_index=False).agg({'result_label':'count'}).rename(columns={'result_label': 'home_wins_per_country'})
+  join_df = pd.merge(grouped_df, grouped_home_df, on='country_id',
+                            how='outer')
+  join_df['percentage_home_win']= join_df.apply(
+      lambda r: np.true_divide(r['home_wins_per_country'], r['matches_per_country']) * 100., axis=1)
+
   countries_df = sql_to_dataframe(conn, select_all_query_table("Country"))
-  for country_id in country_id_to_num_matches:
-    print map_country_to_name(countries_df, country_id).to_string(), " :", str(
-        country_id_to_num_matches[country_id])
+  join_df_name = pd.merge(countries_df, join_df, left_on = 'id', right_on='country_id',
+                          how='outer')
+  return join_df_name[['name', 'percentage_home_win']]
+
 
 
 if __name__ == '__main__':
