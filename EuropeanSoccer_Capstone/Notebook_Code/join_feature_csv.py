110b144081d0f7225167436a24172888a75d5ca0
Sahil <sa.goyal@criteo.com>
2017-11-04 02:18:28 -0700

    Add data files

--- EuropeanSoccer_Capstone/Notebook_Code/join_feature_csv.py
+++ EuropeanSoccer_Capstone/Notebook_Code/join_feature_csv.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-feature_1_df = pd.read_csv('match_features_so_far.csv')
-feature_2_df = pd.read_csv('match_features_so_far_2.csv')
+feature_1_df = pd.read_csv('Data_Structures/ALL_MATCH_FEATURES.csv')
+feature_2_df = pd.read_csv('Data_Structures/form_numbers.csv')
 
 
-JOIN_COLUMNS = ['match_api_id', 'home_team_api_id', 'away_team_api_id',
-                'season', 'date', 'result_label']
+JOIN_COLUMNS = ['match_api_id']
 
 all_feature_df = pd.merge(feature_1_df, feature_2_df,  how='left',
-                  left_on=JOIN_COLUMNS, right_on=JOIN_COLUMNS)
+                  left_on=JOIN_COLUMNS, right_on=JOIN_COLUMNS, suffixes=('', '_y'))
+all_feature_df = all_feature_df[all_feature_df.columns.drop(list(all_feature_df.filter(regex='_y')))]
 
-all_feature_df.to_csv('Data_Structures/MATCH_FEATURES.csv')
+all_feature_df.to_csv('Data_Structures/ALL_MATCH_FEATURES.csv')
