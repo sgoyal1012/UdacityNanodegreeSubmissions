b0bcd49e7a080f624c438e959a5cc263ae713c23
Sahil <sa.goyal@criteo.com>
2017-11-08 17:16:49 -0800

    Note to self: Add draw data

--- EuropeanSoccer_Capstone/Notebook_Code/form_stats.py
+++ EuropeanSoccer_Capstone/Notebook_Code/form_stats.py
@@ -20,6 +20,22 @@ def home_team_all_time_home_record(match_df, full_df):
 
 
 '''
+Function to get the home record (draw rate) for the home team prior to the date today
+'''
+def home_team_all_time_home_draws(match_df, full_df):
+  all_team_home_matches_before_today = full_df[(match_df['date'] >
+                                                full_df['date']) & (full_df['home_team_api_id']==match_df['home_team_api_id'])].shape[0]
+  # Not enough data to go by
+  if all_team_home_matches_before_today == 0:
+    return np.nan
+
+  all_team_home_draws_before_today = full_df[(match_df['date'] >
+                                             full_df['date']) & (full_df['home_team_api_id']==
+                                                                 match_df['home_team_api_id']) & (full_df['result_label'] == "DRAW")].shape[0]
+  return np.true_divide(all_team_home_draws_before_today, all_team_home_matches_before_today)
+
+
+'''
 Function to get the home record (win rate) for the home team prior to this date today; FOR THIS SEASON
 '''
 def home_team_this_season_home_record(match_df, full_df):
@@ -35,6 +51,23 @@ def home_team_this_season_home_record(match_df, full_df):
                                             full_df['date']) & (full_df['home_team_api_id']==match_df['home_team_api_id']) &
                                            (full_df['result_label'] == "HOME_WIN") & (full_df['season'] == match_df['season']) ].shape[0]
   return np.true_divide(all_team_home_wins_this_season, all_team_home_matches_this_season)
+
+'''
+Function to get the home record (home draws) for the home team prior to this date today; FOR THIS SEASON
+'''
+def home_team_this_season_home_draws(match_df, full_df):
+  all_team_home_matches_this_season = full_df[(match_df['date'] >
+                                               full_df['date']) & (full_df['home_team_api_id']==match_df['home_team_api_id']) &
+                                              (full_df['season'] == match_df['season'])].shape[0]
+
+  # Not enough data to go by
+  if all_team_home_matches_this_season == 0:
+    return np.nan
+
+  all_team_home_draws_this_season = full_df[(match_df['date'] >
+                                             full_df['date']) & (full_df['home_team_api_id']==match_df['home_team_api_id']) &
+                                            (full_df['result_label'] == "DRAW") & (full_df['season'] == match_df['season']) ].shape[0]
+  return np.true_divide(all_team_home_draws_this_season, all_team_home_matches_this_season)
 
 
 '''
@@ -56,6 +89,24 @@ def away_team_all_time_away_record(match_df, full_df):
   return np.true_divide(all_team_away_wins_before_today, all_team_away_matches_before_today)
 
 '''
+AWAY TEAM FUNCTIONS
+'''
+'''
+Function to get the away record (win rate) for the away team prior to the date today
+'''
+def away_team_all_time_away_draws(match_df, full_df):
+  all_team_away_matches_before_today = full_df[(match_df['date'] >
+                                                full_df['date']) & (full_df['away_team_api_id']==match_df['away_team_api_id'])].shape[0]
+  # Not enough data to go by
+  if all_team_away_matches_before_today == 0:
+    return np.nan
+
+  all_team_away_draw_before_today = full_df[(match_df['date'] >
+                                             full_df['date']) & (full_df['away_team_api_id']==
+                                                                 match_df['away_team_api_id']) & (full_df['result_label'] == "DRAW")].shape[0]
+  return np.true_divide(all_team_away_draw_before_today, all_team_away_matches_before_today)
+
+'''
 Function to get the away record (win rate) for the away team prior to this date today; FOR THIS SEASON
 '''
 def away_team_this_season_away_record(match_df, full_df):
@@ -73,6 +124,24 @@ def away_team_this_season_away_record(match_df, full_df):
   return np.true_divide(all_team_away_wins_this_season, all_team_away_matches_this_season)
 
 '''
+Function to get the away record (win rate) for the away team prior to this date today; FOR THIS SEASON
+'''
+def away_team_this_season_away_draw(match_df, full_df):
+  all_team_away_matches_this_season = full_df[(match_df['date'] >
+                                               full_df['date']) & (full_df['away_team_api_id']==match_df['away_team_api_id']) &
+                                              (full_df['season'] == match_df['season'])].shape[0]
+
+  # Not enough data to go by
+  if all_team_away_matches_this_season == 0:
+    return np.nan
+
+  all_team_away_draw_this_season = full_df[(match_df['date'] >
+                                            full_df['date']) & (full_df['away_team_api_id']==match_df['away_team_api_id']) &
+                                           (full_df['result_label'] == "DRAW") & (full_df['season'] == match_df['season']) ].shape[0]
+  return np.true_divide(all_team_away_draw_this_season, all_team_away_matches_this_season)
+
+
+'''
 Function to get the away record (win rate) for the away team prior to the date today
 '''
 def away_team_all_time_away_record_at_this_ground(match_df, full_df):
@@ -88,6 +157,22 @@ def away_team_all_time_away_record_at_this_ground(match_df, full_df):
                    (match_df['home_team_api_id'] == full_df['home_team_api_id']) & (full_df['result_label'] == "AWAY_WIN")].shape[0]
   return np.true_divide(all_team_away_wins_before_today_at_this_ground, all_team_away_matches_before_today_at_this_ground)
 
+
+'''
+Function to get the away record (win rate) for the away team prior to the date today
+'''
+def away_team_all_time_away_draws_at_this_ground(match_df, full_df):
+  all_team_away_matches_before_today_at_this_ground = full_df[(match_df['date'] >
+                                                               full_df['date']) & (full_df['away_team_api_id']==match_df['away_team_api_id'])
+                                                              & (match_df['home_team_api_id'] == full_df['home_team_api_id'])].shape[0]
+  # Not enough data to go by
+  if all_team_away_matches_before_today_at_this_ground == 0:
+    return np.nan
+
+  all_team_away_draws_before_today_at_this_ground = full_df[(match_df['date'] >
+                                                            full_df['date']) & (full_df['away_team_api_id']== match_df['away_team_api_id']) &
+                                                           (match_df['home_team_api_id'] == full_df['home_team_api_id']) & (full_df['result_label'] == "DRAW")].shape[0]
+  return np.true_divide(all_team_away_draws_before_today_at_this_ground, all_team_away_matches_before_today_at_this_ground)
 
 '''
 FUNCTION TO GET HEAD TO HEAD in terms of (Home team win percentage, home team lose percentage, draws)
@@ -193,7 +278,7 @@ if __name__ == '__main__':
   home_team_form_guide_this_season = partial(form_guide_this_season, full_df=all_features_df, team_type = "home", LOOKBACK_DAYS = 5)
   match_sample['HOME_TEAM_FORM_GUIDE'] = match_sample.apply(home_team_form_guide_this_season, axis = 1)
 
-  print "Getting away team's form guide for the last five matches of the season""
+  print "Getting away team's form guide for the last five matches of the season"
   away_team_form_guide_this_season = partial(form_guide_this_season, full_df=all_features_df, team_type = "away", LOOKBACK_DAYS = 5)
   match_sample['AWAY_TEAM_FORM_GUIDE'] = match_sample.apply(away_team_form_guide_this_season, axis = 1)
   match_sample.to_csv('ALL_MATCH_FEATURES_2.csv')
